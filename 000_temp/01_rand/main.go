1cc1139aab0767f4c70dbdd648b56adba9bcd98a
jackson lee <31246705+jackson99011@users.noreply.github.com>
2022-11-27 16:27:18 +0800

    basic language practice

--- 000_temp/01_rand/main.go
+++ 000_temp/01_rand/main.go
@@ -1,4 +1,4 @@
-package main
+package test
 
 import (
 	"fmt"
@@ -6,8 +6,32 @@ import (
 	"time"
 )
 
+var z string
+var a int
+
+type hotdog int
+
+var b hotdog
+
+func try() {
+	a = 42
+	b = 43
+	fmt.Println(z)
+	fmt.Println("printing the value of z", z, "ending")
+	fmt.Printf("%T", z)
+	a = 42
+	fmt.Println(a)
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
-	x := rand.Intn(3)
+	x := rand.Intn(100)
 	fmt.Println(x)
+	var y = 100
+	fmt.Println(y)
+	fmt.Println("printing the value of y", y, "ending")
+	fmt.Printf("%T", z)
+	a = 42
+	fmt.Println(a)
+
 }
--- 000_temp/02_hello/main.go
+++ 000_temp/02_hello/main.go
@@ -1,10 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+var x int = 42
+var y string = "john"
+var z bool = true
 
 func main() {
-	x := "Hello, class!"
-	fmt.Println(x)
+	//s := fmt.Sprintf("%v\t%v\t%v\t", x, y, z)
+	//fmt.Println(s)
+	//for i := 33; i < 122; i++ {
+	//	fmt.Printf("%v\t%#x\t%#U\n", i, i, i)
+	//}
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
